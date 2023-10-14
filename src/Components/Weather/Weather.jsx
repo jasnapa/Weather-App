75202c769ae31322c0bcbe8d32d7d4fa3293c89a
jasnapa <jasnajass661@gmail.com>
2023-10-14 17:52:39 +0530

    assets added

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './weather.css'
+import clear from '../Assets/clear.png'
+import cloud from '../Assets/cloud.png'
+import drizzle from '../Assets/drizzle.png'
+import humidity from '../Assets/humidity.png'
+import rain from '../Assets/rain.png'
+import search from '../Assets/search.png'
+import snow from '../Assets/snow.png'
+import wind from '../Assets/wind.png'
 
 const Weather=()=>{
 
     return(
-        <div>
-
+        <div className='container'>
+            <div className="top-bar">
+                <input type="text" className="cityInput" placeholder='search'/>
+                <div className="search">
+                    <img src={search} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
